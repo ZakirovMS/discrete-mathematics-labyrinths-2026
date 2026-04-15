6b676f13049a360ccf0a36cbb318efa7b1f3a3f6
belyaevEDU <belyaev2.vv@edu.spbstu.ru>
2026-04-15 19:17:52 +0300

    feat: added main

---
+++ labyrinth/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include "matrix/matrix.hpp"
+
+int main()
+{
+  zakirov::Matrix maze;
+
+  // Генерируем лабиринт размером 10х10 КОМНАТ (итоговое поле будет 21x21)
+  maze.generateKruskalMaze(10, 10);
+
+  std::cout << "Сгенерированный лабиринт:\n";
+  maze.showMatrix();
+
+  // Запускаем ваш волновой алгоритм для поиска пути
+  maze.waveAlgo();
+
+  std::cout << "\nЛабиринт с просчитанными дистанциями:\n";
+  maze.showTechInfo();
+
+  return 0;
+}
